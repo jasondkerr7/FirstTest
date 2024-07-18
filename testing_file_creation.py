4dc58270570ff804b68799c069192b48208d97da
jasondkerr7 <164694209+jasondkerr7@users.noreply.github.com>
2024-07-18 16:05:36 -0400

    Remove nan's to remove dependency on NumPy

--- testing_file_creation.py
+++ testing_file_creation.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
 t = pd.DataFrame({'Names1':['Jason','Jason','Jacob','Brad','Jason','Jacob','Sean'],
-                  'Names2':[np.nan,'MF','Sichel',np.nan,np.nan,np.nan,'MF'],
-                  'Names3':[np.nan,'Kurz',np.nan,np.nan,np.nan,np.nan,'Kurz'],
                   'Height':[3,1,4,2,0,10,0.5],
-                  'Money':[1,2,3,np.nan,20,101,0.5],
+                  'Money':[1,2,3,12,20,101,0.5],
                   'Rankz':[1,1,1,1,2,3,2]
                     })
 
