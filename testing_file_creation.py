8aca1cece23747b48cc64ae51ac2428c703432bb
jasondkerr7 <164694209+jasondkerr7@users.noreply.github.com>
2024-07-24 16:26:18 -0400

    Testing the Cron Scheduler

--- testing_file_creation.py
+++ testing_file_creation.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-t = pd.DataFrame({'Names1':['Jason','Jason','Jacob','Brad','Jason','Jacob','Sean'],
+t = pd.DataFrame({'Last Names':['Kerr','Kerr','Sichel','Vallido','Kerr','Sichel','Kurz'],
                   'Height':[3,1,4,2,0,10,0.5],
                   'Money':[1,2,3,12,20,101,0.5],
                   'Rankz':[1,1,1,1,2,3,2]
                     })
 
-t.to_csv('file_creation_test_jjbs.csv')
+t.to_csv('file_creation_test_kkvs_cron.csv')
