ba3c0e199313c56c13957958d034b05f0c2c4e6e
jasondkerr7 <164694209+jasondkerr7@users.noreply.github.com>
2024-07-30 15:34:10 -0400

    Test of loading a JSON file as secret

--- testing_file_creation.py
+++ testing_file_creation.py
@@ -1,10 +1,12 @@
 import pandas as pd
+import os
 
 t = pd.DataFrame({'Last Names':['Kerr','Kerr','Sichel','Vallido','Kerr','Sichel','Kurz'],
                   'Height':[3,1,4,2,0,10,0.5],
                   'Money':[1,2,3,12,20,101,0.5],
                   'Rankz':[1,1,1,1,2,3,2]
                     })
-test = pd.read_csv('test_data/test.csv')
 
-print(test)
+cred_json = os.environ['SERVICE_ACCOUNT_CREDENTIALS_JSON']
+
+print(cred_json['client_id'][1:5])
